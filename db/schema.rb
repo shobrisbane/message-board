fd7dd11a0e4bd81c5a90fcb235dac717104bb6a8
shobrisbane <shobrisbane@hotmail.com>
2017-03-05 09:15:19 +0000

    message-board

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170226083613) do
+ActiveRecord::Schema.define(version: 20170305073219) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
