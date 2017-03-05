d86a27972765e6be0207c2785f187628f900c2ae
shobrisbane <shobrisbane@hotmail.com>
2017-03-05 05:13:57 +0000

    message-board

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,24 +1,28 @@
 class MessagesController < ApplicationController
-  def index
-    @message = Message.new
-    # Messageを全て取得する。
-    @messages = Message.all
+  before_action :set_message, only: [:edit, :update]
+  
+  # 中略
+
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
   end
 
-  def create
-    @message = Message.new(message_params)
-    if @message.save
-    redirect_to root_path , notice: 'メッセージを保存しました'
-  else
-    # メッセージが保存できなかった時
-    @messages = Message.all
-    flash.now[:alert] = "メッセージの保存に失敗しました。"
-    render 'index' 
-  end
-end
+  # 中略
+
   private
-  def message_params
-    params.require(:message).permit(:name, :body)
+
+  # 中略
+
+  def set_message
+    @message = Message.find(params[:id])
   end
-  ## ここまで
 end
