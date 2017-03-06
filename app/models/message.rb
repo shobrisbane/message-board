bcf6a683d12b9a5a32f43f11a79e93d49eed4884
shobrisbane <shobrisbane@hotmail.com>
2017-03-06 13:47:16 +0000

    message-board

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,5 +4,5 @@ class Message < ActiveRecord::Base
   # 内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
  #年齢(age)に 0以上かつ150以下の数字
- validates :age , numericality: { greater_than: 0, less_than: 150 } , numericality: true, presence: true
+  validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 150 }
 end
